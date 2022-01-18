000398f21c22864fc696546e9f276539ef8872bf
rogue-dog <keshav.kashik20@nludelhi.ac.in>
2022-01-18 07:51:46 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  var [country,setCountry] = useState("");
+  var c;
+  useEffect(()=>{
+    b();
+  },[]);
+  const b = async ()=>{
+     
+       var a = await fetch("https://ipapi.co/json")
+         .then((res) => res.json())
+         .then((data) => {
+           return data;
+         });
+         setCountry(a['country_name']);
+       
+     
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {country}
     </div>
   );
 }
